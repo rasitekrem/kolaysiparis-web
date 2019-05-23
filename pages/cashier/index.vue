a7b5542bafccec952262cc158361c3ae29c750e9
Raşit Ekrem <rasitekrem@Rasit-MacBook-Air.local>
2019-05-23 03:37:13 +0300

    heroku update

--- pages/cashier/index.vue
+++ pages/cashier/index.vue
@@ -2,6 +2,7 @@
 <div class="container-fluid mr-0 mt-5 col-md-10 " v-if="isAuthority">
     <div class="d-flex justify-content-between mb-3 mt-2">
         <h1 class="text-center">Kasa</h1>
+        <Button class="btn btn-danger">Masa Birleştir</Button>
         <div class="pull-right mr-0 text-right d-sm-down-none small">
             servis edildi&nbsp;<div class="bg-info" style="display: inline;">&nbsp;&nbsp;&nbsp;</div>
             <br>siparişleri mutfakta hazır&nbsp;<div class="bg-warning" style="display: inline;">&nbsp;&nbsp;&nbsp;</div>
